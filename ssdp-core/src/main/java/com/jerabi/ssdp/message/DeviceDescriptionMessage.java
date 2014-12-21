c7f15b821a4d11ca8e6891ede0adcd1acdfff231
Michael Gielda <mgielda@antmicro.com>
2014-12-21 13:32:41 +0100

    Minor header fix

--- ssdp-core/src/main/java/com/jerabi/ssdp/message/DeviceDescriptionMessage.java
+++ ssdp-core/src/main/java/com/jerabi/ssdp/message/DeviceDescriptionMessage.java
@@ -4,8 +4,8 @@ package com.jerabi.ssdp.message;
  * A simple interface that is meant to gather messages that contain 
  * device identifiers and descriptors. E.g: LOCATION, USN, etc. fields. 
  * 
- * It's purpose is to unify handling of such messages.
- * @author luk32
+ * Its purpose is to unify the handling of such messages.
+ * @author lkucharski@antmicro.com
  *
  */
 public interface DeviceDescriptionMessage {
